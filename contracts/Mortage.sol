fe91c0b1b3154e6b38407de281c135800fd988f9
victor <victor_chang@mediconcen.com>
2024-03-23 19:13:50 +0800

    finished base contract

--- contracts/Mortage.sol
+++ contracts/Mortage.sol
@@ -4,12 +4,14 @@ contract MortgageContract {
     address public owner;
     address public thirdPartyPlatform;
     address public leader;
-    uint public usdtAmount;
+    uint public owerUsdtAmount;
+    uint public leaderUsdtAmount;
     bool public contractInitiated;
     bool public ownerAgreed;
     bool public leaderSigned;
     bool public collateralTransferred;
     string public requirement;
+    uint public expirationTime;
 
     mapping(address => uint256) public ownerBalance;
     mapping(address => uint256) public thirdPartyPlatformBalance;
@@ -20,9 +22,13 @@ contract MortgageContract {
         address indexed thirdPartyPlatform,
         string indexed requirementAndAmount
     );
-    event ContractApproved(address indexed owner, uint256 indexed usdtAmount);
+    event ContractApproved(
+        address indexed owner,
+        uint256 indexed owerUsdtAmount
+    );
     event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Escrowed(address indexed escrowAddress, address indexed escrowOwner);
+    event Escrowed(address indexed escrowLeader, address indexed escrowOwner);
+    event Signed(address indexed leader);
 
     modifier onlyOwner() {
         require(
@@ -52,18 +58,21 @@ contract MortgageContract {
         address _owner,
         address _thirdPartyPlatform,
         address _leader,
-        uint _usdtAmount,
-        string memory _requirement
+        uint _owerUsdtAmount,
+        string memory _requirement,
+        uint _expirationTime
     ) {
         owner = _owner;
         thirdPartyPlatform = _thirdPartyPlatform;
         leader = _leader;
-        usdtAmount = _usdtAmount;
+        owerUsdtAmount = _owerUsdtAmount;
+        leaderUsdtAmount = _owerUsdtAmount / 2;
         requirement = _requirement;
         contractInitiated = false;
         ownerAgreed = false;
         leaderSigned = false;
         collateralTransferred = false;
+        expirationTime = _expirationTime;
     }
 
     function initiateContract() external onlyThirdPartyPlatform {
@@ -72,11 +81,11 @@ contract MortgageContract {
         string memory descriptionOfrequirment = string(
             abi.encodePacked("reauirment is : ", requirement)
         );
-        string memory usdtAmount = string(
-            abi.encodePacked("usdt amount is : ", usdtAmount)
+        string memory owerUsdtAmount = string(
+            abi.encodePacked("usdt amount is : ", owerUsdtAmount)
         );
         string memory requirementAndAmount = string(
-            abi.encodePacked(descriptionOfrequirment, usdtAmount)
+            abi.encodePacked(descriptionOfrequirment, owerUsdtAmount)
         );
 
         emit ContractInitiated(owner, thirdPartyPlatform, requirementAndAmount); // 發送通知給業主以及第三方平台
@@ -84,31 +93,38 @@ contract MortgageContract {
         contractInitiated = true;
     }
 
-    function transferCollateralToThridPartyPlatform() external onlyOwner {
+    function transferCollateralToThridPartyPlatform(
+        bool _isOnwerAgreed
+    ) external onlyOwner {
+        require(_isOnwerAgreed, "Onwer need granted access ");
+        ownerAgreed = _isOnwerAgreed;
+        emit Transfer(owner, thirdPartyPlatform, owerUsdtAmount);
+
+        thirdPartyPlatformBalance[thirdPartyPlatform] += owerUsdtAmount;
+
+        ownerBalance[owner] -= owerUsdtAmount;
+
+        owerUsdtAmount = 0;
+
+        emit ContractApproved(owner, owerUsdtAmount);
+    }
+
+    function requestLeaderSignature(
+        bool _isLeaderAgreed
+    ) external onlyThirdPartyPlatform {
         require(ownerAgreed, "Owner has not agreed to the contract");
+        require(!leaderSigned, "Leader has already signed");
+        require(_isLeaderAgreed, "Leader need granted access ");
 
-        // 將抵押品轉移到第三方平台錢包
-        emit ContractApproved();
-    }
+        leaderSigned = _isLeaderAgreed;
+        emit Escrowed(leader, owner);
 
-    function agreeToContract() external onlyOwner {
-        require(contractInitiated, "Contract has not been initiated yet");
-        ownerAgreed = true;
-    }
+        emit Transfer(leader, thirdPartyPlatform, leaderUsdtAmount);
 
-    function requestLeaderSignature() external onlyOwner {
-        require(ownerAgreed, "Owner has not agreed to the contract");
-        require(!leaderSigned, "Leader has already signed the contract");
-        // 請求 leader 簽名
-        leaderSigned = true;
-    }
+        thirdPartyPlatformBalance[thirdPartyPlatform] += leaderUsdtAmount;
+        leaderBalance[leader] -= leaderUsdtAmount;
 
-    function transferCollateral() external onlyLeader {
-        require(leaderSigned, "Leader has not signed the contract yet");
-        // 檢查抵押品是否符合資格
-        // 如果符合資格，則將抵押品轉移到第三方平台錢包
-        // 否則，將抵押品退回給業主
-
-        collateralTransferred = true; // 假設這裡成功地完成了抵押品的轉移
+        leaderUsdtAmount = 0;
+        emit Signed(leader);
     }
 }
